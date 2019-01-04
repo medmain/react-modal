41dce1702f3b73e3bb88d5aa9286a26d0f74f5e6
michaelrambeau <mikeair@gmail.com>
2019-01-04 17:21:17 +0900

    Use `minimatch` instead of React context to manage the state

--- src/medmain-modal/modal-manager.js
+++ src/medmain-modal/modal-manager.js
@@ -1,85 +1,60 @@
-/*
-For compatibility with the existing API,
-a class that provides the following method:
-- createElement
-- confirm
-- alert
-- dialog
-*/
 import React from 'react';
+import Base from '@ministate/base';
+import subscribe from '@ministate/react';
 
-import {ModalProvider, ModalConsumer} from './modal-context';
-import {Confirm, Alert, Dialog, Modal} from './components';
+import ModalStack from "./modal-stack";
+import {Alert, Confirm, Dialog, Modal} from './components';
 
-class ModalManager {
-  constructor({okButtonTitle, cancelButtonTitle} = {}) {
-    this.options = {
-      okButtonTitle,
-      cancelButtonTitle
-    };
-  }
+class ModalManager extends Base {
+  state = {
+    stack: []
+  };
   createElement() {
-    const {okButtonTitle, cancelButtonTitle} = this.options;
-    const enhance = withProps({okButtonTitle, cancelButtonTitle});
-    return (
-      <ModalProvider>
-        <ModalConsumer>
-          {({showModal /*hideModal*/}) => {
-            // Attach the modal methods now that we have access to `showModal` function
-            this.alert = showComponent({
-              Component: enhance(Alert),
-              showModal
-            });
-            this.confirm = showComponent({
-              Component: enhance(Confirm),
-              showModal
-            });
-            this.dialog = showDialog({
-              showModal
-            });
-          }}
-        </ModalConsumer>
-      </ModalProvider>
-    );
+    const Stack = subscribe(this)(ModalStack);
+    return <Stack modalManager={this} />;
   }
-}
-
-const withProps = extraProps => Wrapped => props => Wrapped({...props, ...extraProps});
-
-const showComponent = ({Component, showModal}) => (message, props = {}) => {
-  const style = setModalStyle(props);
-  return new Promise(resolve => {
-    return showModal(Component, {message, style, ...props, onClose: value => resolve(value)});
-  });
-};
-const showDialog = ({showModal}) => (props = {}) => {
-  const style = setModalStyle(props);
-  const {render: CustomDialog} = props; // `render` attribute can be used to render a custom dialog component
-  return new Promise(resolve => {
+  open({component, props}) {
+    return new Promise(resolve => {
+      this.setState({
+        stack: [...this.state.stack, {component, props}]
+      });
+    });
+  }
+  close() {
+    if (this.state.stack.length) {
+      this.setState({
+        stack: this.state.stack.slice(0, -1)
+      });
+    }
+  }
+  dialog({render: CustomDialog, ...options}) {
+    // if `render` attribute is used, a custom component is used instead of the default `Dialog` component
     const Component = CustomDialog
       ? ({onClose}) => (
-          <Modal onClose={() => console.log('close')}>
-            <CustomDialog
-              close={value => {
-                resolve(onClose(value))
-              }}
-            />
+          <Modal>
+            <CustomDialog close={value => onClose(value)} />
           </Modal>
         )
       : Dialog;
-    return showModal(Component, {style, ...props, onClose: value => resolve(value)});
-  });
-};
-
-/*
-Convert some optional attributes related to the styling (width, padding...)
-into the style props applied to ReactModal
-*/
-const setModalStyle = props => {
-  const contentProps = ['width', 'padding'];
-  const reducer = (acc, val) => (props[val] ? {...acc, [val]: props[val]} : acc);
-  const content = contentProps.reduce(reducer, {});
-  return {content};
-};
+    return this._showComponent(Component, options);
+  }
+  alert(message, options) {
+    return this._showComponent(Alert, {...options, message});
+  }
+  confirm(message, options) {
+    return this._showComponent(Confirm, {...options, message});
+  }
+  /*
+  Private method used by alert(), confirm() and dialog()
+  */
+  _showComponent(component, props) {
+    return new Promise(resolve => {
+      this.open({
+        component,
+        props: {onClose: value => resolve(value), ...props}
+      });
+    });
+  }
+}
 
 export default ModalManager;
--- src/medmain-modal/modal-stack.js
+++ src/medmain-modal/modal-stack.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-const ModalStack = ({stack, hideModal}) =>
-  stack.length
+const ModalStack = ({modalManager}) => {
+  const stack = modalManager.state.stack;
+  return stack.length
     ? stack.map(({component: Component, props}, index) => {
         const {width} = props;
         const style = width ? {content: {width}} : {};
         const {onClose} = props;
         const mergedOnClose = value => {
-          hideModal();
+          modalManager.close();
           onClose(value);
         };
         return <Component {...props} onClose={mergedOnClose} style={style} key={index} />;
       })
     : null;
+};
 
 export default ModalStack;
